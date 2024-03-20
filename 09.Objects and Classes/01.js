8f1f5c9716eb903c8114ddd7d630cc2c306be1c6
Kristiyan Omayski <122819613+Omayski13@users.noreply.github.com>
2024-03-20 22:40:42 +0200

    Create 01.js

---
+++ 09.Objects and Classes/01.js
@@ -0,0 +1,19 @@
+function personalInfo(firstName,lastName,age){
+    let person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    }
+
+
+    return person
+}
+
+console.log(personalInfo("Peter", 
+"Pan",
+"20"
+))
+console.log(personalInfo("George", 
+"Smith",
+"18"
+))
