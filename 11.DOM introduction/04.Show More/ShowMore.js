4fec7874e44d378af600d49a9f44ab155a22d8b7
Kristiyan Omayski <122819613+Omayski13@users.noreply.github.com>
2024-03-26 17:18:31 +0200

    Create ShowMore.js

---
+++ 11.DOM introduction/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const hiddenText = document.getElementById('text')
+    hiddenText.style.display = 'inline'
+    const readMore = document.getElementById('more')
+    readMore.style.display = 'none'
+}
