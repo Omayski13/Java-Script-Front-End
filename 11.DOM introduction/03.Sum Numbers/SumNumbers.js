c8cc8028e952bb63f046fc42032b820eb17c87bd
Kristiyan Omayski <122819613+Omayski13@users.noreply.github.com>
2024-03-26 17:18:24 +0200

    Create SumNumbers.js

---
+++ 11.DOM introduction/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,7 @@
+function calc() {
+    result = document.getElementById('sum')
+    const firstNum = Number(document.getElementById('num1').value)
+    const secondNum = Number(document.getElementById('num2').value)
+    result.value = firstNum + secondNum
+
+}
