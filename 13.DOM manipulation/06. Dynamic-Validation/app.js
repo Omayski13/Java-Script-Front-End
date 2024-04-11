8848183e73d83bdfb6b958a21b47af8b998e5403
Kristiyan Omayski <122819613+Omayski13@users.noreply.github.com>
2024-04-11 22:45:27 +0300

    Create app.js

---
+++ 13.DOM manipulation/06. Dynamic-Validation/app.js
@@ -0,0 +1,17 @@
+function validate() {
+    function validate() {
+        let inputField = document.getElementById('email')
+        inputField.addEventListener('change', onChangeEvent)
+    
+        let regexPattern = /[a-z]+@[a-z]+\.[a-z]+/g
+    
+        function onChangeEvent(e) {
+            let currentValue = e.currentTarget.value
+            if (!currentValue.match(regexPattern)) {
+                e.currentTarget.className = 'error'
+            } else {
+                e.currentTarget.classList.remove('error')
+            }
+        }
+    }
+}
